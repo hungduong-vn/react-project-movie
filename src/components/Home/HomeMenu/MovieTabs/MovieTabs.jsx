a25d2ef391647c4d9fb69e3473f03214dc841a0a
Jasper-Duong <duongngochung13@gmail.com>
2022-08-29 17:04:03 +0700

    Add MovieShowtimes

--- src/components/Home/HomeMenu/MovieTabs/MovieTabs.jsx
+++ src/components/Home/HomeMenu/MovieTabs/MovieTabs.jsx
@@ -3,24 +3,23 @@ import React from "react";
 import { filterShowtimesByDay } from "../../../../utils/helper";
 import MovieTabPane from "../MovieTabPane/MovieTabPane";
 import ShowtimeBtn from "../ShowtimeBtn/ShowtimeBtn";
-import ShowtimeTab from "../ShowtimeBtn/ShowtimeBtn";
 
 const { TabPane } = Tabs;
+export const renderShowtimes = (showtimeList) => {
+  const showtimes = filterShowtimesByDay(showtimeList);
+  return showtimes.map((ele, idx) => (
+    <div
+      className={`showtime py-3${idx === 0 ? " firstShow" : ""}`}
+      key={ele.date}
+    >
+      <h1 className="pl-1">{ele.date}</h1>
+      {ele.timeList.map((time, idxTime) => (
+        <ShowtimeBtn time={time} key={idxTime} />
+      ))}
+    </div>
+  ));
+};
 export default function MovieTabs({ movieList }) {
-  const renderShowtimes = (showtimeList) => {
-    const showtimes = filterShowtimesByDay(showtimeList);
-    return showtimes.map((ele, idx) => (
-      <div
-        className={`showtime py-3${idx === 0 ? " firstShow" : ""}`}
-        key={ele.date}
-      >
-        <h1 className="pl-1">{ele.date}</h1>
-        {ele.timeList.map((time, idxTime) => (
-          <ShowtimeBtn time={time} key={idxTime} />
-        ))}
-      </div>
-    ));
-  };
   return (
     <Tabs tabPosition="left" style={{ height: 600 }}>
       {movieList.map((film) => {
--- src/components/MovieDetail/MovieShowtimes/MovieShowtimes.jsx
+++ src/components/MovieDetail/MovieShowtimes/MovieShowtimes.jsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import { Tabs } from "antd";
+import React from "react";
+import BrandTabPane from "../../Home/HomeMenu/BrandTabPane/BrandTabPane";
+import CinemaTabPane from "../../Home/HomeMenu/CinemaTabPane/CinemaTabPane";
+import CinemaTabs from "../../Home/HomeMenu/CinemaTabs/CinemaTabs";
+import { renderShowtimes } from "../../Home/HomeMenu/MovieTabs/MovieTabs";
 
-export default function MovieShowtimes() {
+const { TabPane } = Tabs;
+export default function MovieShowtimes({ showtimes }) {
+  console.log(showtimes);
   return (
-    <div className='container'>MovieShowtimes</div>
-  )
+    <div className="container">
+      <Tabs tabPosition="left" style={{ height: "600px" }}>
+        {showtimes.map((ele) => {
+          return (
+            <TabPane
+              tab={
+                <BrandTabPane
+                  brands={{ logo: ele.logo, tenCumRap: ele.tenHeThongRap }}
+                />
+              }
+              key={ele.maHeThongRap}
+            >
+              <Tabs tabPosition="left" style={{ height: "600px" }}>
+                {ele.cumRapChieu.map((cinema) => {
+                  return (
+                    <TabPane
+                      tab={<CinemaTabPane cinema={cinema} logo={ele.logo} />}
+                      key={cinema.maCumRap}
+                    >
+                      {renderShowtimes(cinema.lichChieuPhim)}
+                    </TabPane>
+                  );
+                })}
+              </Tabs>
+            </TabPane>
+          );
+        })}
+      </Tabs>
+    </div>
+  );
 }
