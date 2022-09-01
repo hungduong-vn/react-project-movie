ae535ccd6934d33e3c42937fd6dd78e0655121f6
Jasper-Duong <duongngochung13@gmail.com>
2022-09-01 21:56:53 +0700

    Fix bug call api

--- src/services/axios.configs.js
+++ src/services/axios.configs.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { TOKEN_CYBERSOFT } from "../constants/common";
 
 export const request = axios.create({
-  baseURL: "http://movienew.cybersoft.edu.vn/api/",
+  baseURL: "https://movienew.cybersoft.edu.vn/api/",
   headers: {
     TokenCybersoft: TOKEN_CYBERSOFT,
   },
