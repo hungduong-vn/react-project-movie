9f4031b9c49302668a762989c46d1ad0f5d462c7
Jasper-Duong <duongngochung13@gmail.com>
2022-09-11 23:37:22 +0700

    Fine tune UI

--- src/components/Home/HomeMovies/HomeMoviesMultiple.jsx
+++ src/components/Home/HomeMovies/HomeMoviesMultiple.jsx
@@ -3,44 +3,35 @@ import Slider from "react-slick";
 import { isImgFit } from "../../../utils/helper";
 import HomeMovie from "../HomeMovie/HomeMovie";
 
-export default class HomeMoviesMultiple extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { movies: [] };
-  }
-  render() {
-    const sortFitImg = async (movies) => {
-      const result = [];
-      for (let movie of movies) {
-        if (await isImgFit(movie.hinhAnh)) {
-          result.push(movie);
-        }
+export default function HomeMoviesMultiple({ movieList }) {
+  const sortFitImg = async (movies) => {
+    const result = [];
+    for (let movie of movies) {
+      if (await isImgFit(movie.hinhAnh)) {
+        result.push(movie);
       }
-      this.setState({ movies: [...result] });
-      // console.log("Sorted Fit Movies:", result);
-      return result;
-    };
-    const { movieList } = this.props;
-    // console.log({ movieList });
-    const settings = {
-      // className: "center",
-      centerMode: true,
-      infinite: true,
-      // centerPadding: "60px",
-      slidesToShow: 4,
-      speed: 500,
-      rows: 2,
-      slidesPerRow: 1,
-    };
-    console.log(this.state);
-    return (
-      <div>
-        <Slider {...settings}>
-          {movieList.map((ele) => {
-            return <HomeMovie movie={ele} key={ele.maPhim} />;
-          })}
-        </Slider>
-      </div>
-    );
-  }
+    }
+    // console.log("Sorted Fit Movies:", result);
+    return result;
+  };
+  // console.log({ movieList });
+  const settings = {
+    // className: "center",
+    centerMode: true,
+    infinite: true,
+    // centerPadding: "60px",
+    slidesToShow: 4,
+    speed: 500,
+    rows: 2,
+    slidesPerRow: 1,
+  };
+  return (
+    <div>
+      <Slider {...settings}>
+        {movieList.map((ele) => {
+          return <HomeMovie movie={ele} key={ele.maPhim} />;
+        })}
+      </Slider>
+    </div>
+  );
 }
