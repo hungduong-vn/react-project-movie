db550558a417afb972bfe88c907016613cf41599
Jasper-Duong <duongngochung13@gmail.com>
2022-09-14 13:56:01 +0700

    Add News, Contact placeholder page

--- src/pages/Home/Contact/Contact.jsx
+++ src/pages/Home/Contact/Contact.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Contact() {
-  return (
-    <div>Contact</div>
-  )
+  return <div className="row justify-center py-5 signIn__header">CONTACT</div>;
 }
--- src/pages/Home/News/News.jsx
+++ src/pages/Home/News/News.jsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function News() {
-  return (
-    <div>News</div>
-  )
+  return <div className="row justify-center py-5 signIn__header">NEWS</div>;
 }
